df0ba214e52d285c73cdb8462b98246ff13de9ff
Khris Punzalan <khrispunzalan@gmail.com>
2020-02-01 20:15:42 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+// has a functional expression called divide:
+
+function divide() {
+    return 2000 / 100
+}
+
+// has an arrow function called square:
+// let square = (num) => num * num
+let square = num => num * num
+
+// has an arrow function called add
+// add arrow function takes two parameters and adds them together
+let add = (param1, param2) => param1 + param2
